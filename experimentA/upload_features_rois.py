b68e95d759279b4b2fdf64a31d62ea31451c09ac
Simon Li <spli@dundee.ac.uk>
2019-06-04 15:44:55 +0100

    Save ROIs

--- experimentA/upload_features_rois.py
+++ experimentA/upload_features_rois.py
@@ -5,23 +5,36 @@
 # exists. If you want to use a different login ensure you logout first.
 
 from glob import glob
+import numpy as np
 import os
 import pandas as pd
+from skimage.io import imread
 
 import omero.clients
 import omero.cli
 from omero.rtypes import (
     rstring,
-    unwrap,
+)
+from omero_rois import (
+    mask_from_binary_image,
+    NoMaskFound,
 )
 from omero_upload import upload_ln_s
 
 DRYRUN = False
+SKIP_IF_EXISTING_ROIS = True
+PROJECT_NAME = 'idr0062-blin-nuclearsegmentation/experimentA'
 OMERO_DATA_DIR = '/data/OMERO'
 NAMESPACE = 'openmicroscopy.org/idr/analysis/original'
+RGBA = (128, 128, 128, 128)
 
 
 def get_feature_files():
+    """
+    Returns mappings of:
+    - image names to manual segmentations
+    - dataset names to feature TSVs
+    """
     fileset_root = (
         '/uod/idr/filesets/idr0062-blin-nuclearsegmentation/20190429-ftp')
     labels = sorted(glob('*_labels.txt'))
@@ -60,13 +73,18 @@ def get_feature_files():
 
 
 def get_datasets(conn):
-    project = conn.getObject('Project', attributes={
-        'name': 'idr0062-blin-nuclearsegmentation/experimentA'})
+    """
+    Get all datasets
+    """
+    project = conn.getObject('Project', attributes={'name': PROJECT_NAME})
     for dataset in project.listChildren():
         yield dataset
 
 
 def get_images(conn):
+    """
+    Get all images
+    """
     for dataset in get_datasets(conn):
         for image in dataset.listChildren():
             if not image.name.endswith('_Manual.tif'):
@@ -74,6 +92,9 @@ def get_images(conn):
 
 
 def existing_file_attachments(obj):
+    """
+    Get existing file annotations
+    """
     files = set()
     for ann in obj.listAnnotations():
         try:
@@ -82,6 +103,66 @@ def existing_file_attachments(obj):
         except AttributeError:
             continue
     return files
+
+
+def get_labels(im, image_attachment_map):
+    """
+    Get the manual segmentation for an image
+    """
+    # Currently broken until https://github.com/ome/omero-server/pull/16
+    # is deployed to the IDR
+    # anns = [a for a in im.listAnnotations(NAMESPACE)
+    #         if a.getFile().name.endswith('_Manual.tif')]
+    # assert len(anns) == 1
+    # with anns[0].getFile().asFileObj() as fo:
+    #     seg = imread(fo)
+    seg = imread(image_attachment_map[im.name])
+    segint = seg.astype(int)
+    assert np.all(np.equal(seg, segint))
+    assert segint.min() == 0
+    return segint
+
+
+def create_rois(im, labels):
+    """
+    Create single-Z masks for all labels and Zs.
+    Combine all Zs for a given label into one ROI.
+    """
+    nz = im.getSizeZ()
+    assert im.getSizeT() == 1
+    assert all(np.equal(labels.shape,
+                        [im.getSizeZ(), im.getSizeY(), im.getSizeX()]))
+
+    rois = []
+    ulabels = np.unique(labels)
+    for n in range(1, len(ulabels)):
+        print('Creating ROI index {}'.format(n))
+        roi = omero.model.RoiI()
+        roi.setName(rstring(n))
+        for z in range(nz):
+            try:
+                mask = mask_from_binary_image(
+                    labels[z] == ulabels[n], rgba=RGBA, z=z)
+                roi.addShape(mask)
+            except NoMaskFound:
+                pass
+        rois.append(roi)
+
+    return rois
+
+
+def save_rois(conn, im, rois):
+    """
+    Save ROIs on an image
+    """
+    print('Saving %d ROIs for image %d:%s' % (len(rois), im.id, im.name))
+    us = conn.getUpdateService()
+    for roi in rois:
+        # Due to a bug need to reload the image for each ROI
+        im = conn.getObject('Image', im.id)
+        roi.setImage(im._obj)
+        roi1 = us.saveAndReturnObject(roi)
+        assert roi1
 
 
 def main(conn):
@@ -135,6 +216,20 @@ def main(conn):
             link.setParent(ds._obj)
             link.setChild(fa)
             link = conn.getUpdateService().saveAndReturnObject(link)
+
+    roisvc = conn.getRoiService()
+    for im in get_images(conn):
+        if SKIP_IF_EXISTING_ROIS and roisvc.findByImage(im.id, None).rois:
+            print('Image:{} {} has ROIs, skipping'.format(im.id, im.name))
+            continue
+        print('Image:{} {}'.format(im.id, im.name))
+        try:
+            labels = get_labels(im, image_attachment_map)
+            rois = create_rois(im, labels)
+            if not DRYRUN:
+                save_rois(conn, im, rois)
+        except KeyError:
+            print('No segmentation found for {}'.format(im.name))
 
 
 if __name__ == '__main__':
